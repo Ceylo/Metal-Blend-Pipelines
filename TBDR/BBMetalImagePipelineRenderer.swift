1d55445f0d2658cb6bba2cc803e5dea385a5fc2d
Ceylo <lucas.soltic@orange.fr>
2024-06-05 17:34:51 +0200

    Add dummy file for BBMetalImage renderer

---
+++ TBDR/BBMetalImagePipelineRenderer.swift
@@ -0,0 +1,12 @@
+//
+//  BBMetalImagePipelineRenderer.swift
+//  TBDR
+//
+//  Created by Ceylo on 05/06/2024.
+//
+
+
+// https://github.com/Silence-GitHub/BBMetalImage
+// Skipped because:
+// - demo project crashes when trying "Multiple images blend" example
+// - only iOS is supported out of the box
